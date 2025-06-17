c1bca8a52640f8e542c8c21cf45f41cc3635fc1b
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-17 20:01:00 +0400

    feat: add seller-home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {SellerAuthComponent} from './components/seller-auth/seller-auth.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
+import {SellerHomeComponent} from './components/seller-home/seller-home.component';
 
 export const routes: Routes = [
   {component: HomeComponent, path: ''},
   {component: SellerAuthComponent, path: 'seller-auth'},
-  {component: NotFoundComponent, path: '**'}
+  {component: SellerHomeComponent, path: 'seller-home'},
+  {component: NotFoundComponent, path: '**'},
 ];
