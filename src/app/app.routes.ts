259ae61722556a676b4cdbff6fc06365aa4423f5
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-17 17:18:17 +0400

    feat: add base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {SellerAuthComponent} from './components/seller-auth/seller-auth.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {component: HomeComponent, path: ''},
+  {component: SellerAuthComponent, path: 'seller-auth'},
+  {component: NotFoundComponent, path: '**'}
+];
