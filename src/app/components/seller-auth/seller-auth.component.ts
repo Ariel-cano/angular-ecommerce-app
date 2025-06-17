6ba45a52ab4f8045a0fc86c03799f560ffd3f979
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-17 23:03:48 +0400

    feat: add login form

--- src/app/components/seller-auth/seller-auth.component.ts
+++ src/app/components/seller-auth/seller-auth.component.ts
@@ -2,17 +2,20 @@ import {Component, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {SellerService} from '../../services/seller.service';
 import {signUp} from '../../models/data-types';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.scss'
 })
 export class SellerAuthComponent implements OnInit{
+  showLogin : boolean = true;
 
   constructor(private seller: SellerService) {
   }
@@ -20,9 +23,18 @@ export class SellerAuthComponent implements OnInit{
   ngOnInit() {
     this.seller.reloadSeller();
   }
+  openLogin(){
+    this.showLogin = true;
+  }
+  openSignUp(){
+    this.showLogin = false;
+  }
 
   signUp(data: signUp){
     console.warn(data);
     this.seller.userSignUp(data);
+  }
+  login(data: signUp){
+    console.warn(data);
   }
 }
