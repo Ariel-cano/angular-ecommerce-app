8c2196f4ea8a1736bd0f22931dd90dc1bc019365
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-18 13:44:36 +0400

    feat: add submit product method

--- src/app/components/seller-add-product/seller-add-product.component.ts
+++ src/app/components/seller-add-product/seller-add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ProductService} from '../../services/product.service';
+import {Product} from '../../models/data-types';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -11,8 +13,18 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './seller-add-product.component.scss'
 })
 export class SellerAddProductComponent {
+  addProductMessage: string | undefined;
+  constructor(private productSrc: ProductService) {
+  }
 
-  submitProduct(data: object){
-    console.log(data);
+  submitProduct(data: Product){
+    this.productSrc.addProduct(data).subscribe((result)=>{
+      if (result){
+        this.addProductMessage = "Product added successfully";
+      }
+    });
+    setTimeout(() => {
+      this.addProductMessage = undefined;
+    }, 3000);
   }
 }
