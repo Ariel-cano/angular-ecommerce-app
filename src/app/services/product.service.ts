d944f300d678f14f0f60e92f9035f4c6473366fa
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-18 13:44:05 +0400

    feat: add product service and interface

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Product} from '../models/data-types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  baseUrl = 'http://localhost:3000/products'
+
+  constructor(private http : HttpClient) { }
+
+  addProduct(product: Product){
+    return this.http.post(this.baseUrl, product);
+  }
+}
