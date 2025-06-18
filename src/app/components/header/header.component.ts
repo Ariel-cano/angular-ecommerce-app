fe135391329d402547521d0ae8719204aacf73c8
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-18 12:14:27 +0400

    feat: add new menu type

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {NgIf, NgSwitch, NgSwitchCase} from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    NgIf,
+    RouterLink,
+    NgSwitch,
+    NgSwitchCase
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  menuType: string = 'default';
+
+  constructor(private route: Router) {
+
+  }
+
+  ngOnInit() {
+    this.route.events.subscribe((val: any) => {
+        if (val.url) {
+          if (localStorage.getItem('seller') && val.url.includes('seller')) {
+            this.menuType = 'seller';
+          } else {
+            this.menuType = 'default';
+          }
+        }
+      }
+    );
+  }
 
 }
