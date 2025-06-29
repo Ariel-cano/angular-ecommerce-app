7db30a3dd943aa5a4d93f7d5ecbd1e7f0d29d62f
Ariel-cano <vaskaivanov856@gmail.com>
2025-06-29 15:52:08 +0400

    feat: add delete cart Items and orderList methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -97,6 +97,17 @@ export class ProductService {
   orderNow(data: order){
     return this.http.post(`http://localhost:3000/orders`,data);
   }
+  orderList(){
+    let userStore = localStorage.getItem('user');
+    let userData = userStore && JSON.parse(userStore);
+    return this.http.get<order[]>('http://localhost:3000/orders?userId='+ userData.id);
+  }
+  deleteCartItems(cartId: string){
+    return this.http.delete(`http://localhost:3000/cart/${cartId}`).subscribe((result)=>{
+      this.cartInfo.emit([]);
+      this.cartData.set(0);
+    });
+  }
 
 
 }
