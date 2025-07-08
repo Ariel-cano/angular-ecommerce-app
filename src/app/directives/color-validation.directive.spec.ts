997604cf65a8732f392df7ce4876d4550ec4f2e9
Ariel-cano <vaskaivanov856@gmail.com>
2025-07-08 17:34:37 +0400

    feat: add ColorValidationDirective and basic test
    Introduces a new Angular directive, ColorValidationDirective, with a corresponding unit test to verify its instantiation.

---
+++ src/app/directives/color-validation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ColorValidationDirective } from './color-validation.directive';
+
+describe('ColorValidationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ColorValidationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
