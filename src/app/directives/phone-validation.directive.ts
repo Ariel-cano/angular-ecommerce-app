884e5cd90e397c9ef67716c263a4aab615c99f81
Ariel-cano <vaskaivanov856@gmail.com>
2025-07-14 21:41:40 +0400

    feat: Implement Validator interface in phone validation directive
    The PhoneValidationDirective now implements the Validator interface and registers itself as an NG_VALIDATOR provider. This enables Angular forms to use the custom phone validation logic defined in CustomValidators.phoneValidator.

--- src/app/directives/phone-validation.directive.ts
+++ src/app/directives/phone-validation.directive.ts
@@ -1,11 +1,21 @@
-import { Directive } from '@angular/core';
+import {Directive, forwardRef} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
+import {CustomValidators} from '../validators/custom-validators';
 
 @Directive({
   selector: '[appPhoneValidation]',
-  standalone: true
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => PhoneValidationDirective),
+      multi: true
+    }
+  ]
 })
-export class PhoneValidationDirective {
-
-  constructor() { }
+export class PhoneValidationDirective implements Validator{
+  validate(control: AbstractControl): ValidationErrors | null {
+    return CustomValidators.phoneValidator(control);
+  }
 
 }
