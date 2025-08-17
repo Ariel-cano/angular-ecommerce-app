4dcc88ea0baf2090f5c3240f2b909e5b364175cd
Ariel-cano <vaskaivanov856@gmail.com>
2025-08-17 13:31:06 +0400

    feat: add validation in addProduct form

--- src/app/components/seller-add-product/seller-add-product.component.ts
+++ src/app/components/seller-add-product/seller-add-product.component.ts
@@ -3,12 +3,21 @@ import {FormsModule} from '@angular/forms';
 import {ProductService} from '../../services/product.service';
 import {Product} from '../../models/data-types';
 import {Router} from '@angular/router';
+import {UrlValidationDirective} from '../../directives/url-validation.directive';
+import {NgIf, NgStyle} from '@angular/common';
+import {NameValidationDirective} from '../../directives/name-validation.directive';
+import {ColorValidationDirective} from '../../directives/color-validation.directive';
 
 @Component({
   selector: 'app-seller-add-product',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    UrlValidationDirective,
+    NgIf,
+    NgStyle,
+    NameValidationDirective,
+    ColorValidationDirective
   ],
   templateUrl: './seller-add-product.component.html',
   styleUrl: './seller-add-product.component.scss'
