69d63f6ea632e9d4d5ab72bb4580635be3b2400e
Ariel-cano <vaskaivanov856@gmail.com>
2025-08-17 13:28:05 +0400

    feat: Implement Validator interface in color validation directive

--- src/app/directives/color-validation.directive.ts
+++ src/app/directives/color-validation.directive.ts
@@ -1,11 +1,20 @@
-import { Directive } from '@angular/core';
+import {Directive, forwardRef} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
+import {CustomValidators} from '../validators/custom-validators';
 
 @Directive({
   selector: '[appColorValidation]',
-  standalone: true
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => ColorValidationDirective),
+      multi: true
+    }
+  ]
 })
-export class ColorValidationDirective {
-
-  constructor() { }
-
+export class ColorValidationDirective implements Validator{
+  validate(control: AbstractControl): ValidationErrors | null {
+    return CustomValidators.colorValidator(control);
+  }
 }
